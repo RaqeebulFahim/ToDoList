7a62a27568e7cfcd41b041741fa8eb4d4d9f2132
raqeebulfahim <raqeebulfahim@gmail.com>
2025-05-10 23:49:48 +0600

    todolist

---
+++ todolist.js
@@ -0,0 +1,37 @@
+// const Tasklist=[
+//     { TaskName: "Prayer"},
+//     { TaskName: "Cooking"},
+// ]
+
+// let liList='';
+// Tasklist.forEach(task=>{
+//     liList+=`<li>${task.TaskName}</li>`;
+// });
+
+// let printtasks=document.getElementById('tasklist');
+// // console.log(printtasks);
+// printtasks.innerHTML=liList;
+
+
+let add= document.getElementById('add');
+add.addEventListener('click',(e)=>{
+    e.preventDefault();
+    let newtask=document.getElementById('newtask');
+    let newList=document.getElementById('newList');
+
+    console.log(newtask);
+
+    newList.innerHTML+=`<span class="d-flex align-items-end"><li class="m-0">${newtask.value} </li> <button class="del btn btn-sm btn-danger" style=" color: white; ">X</button></span>`;
+    newtask.value='';
+
+    let del=document.querySelectorAll(".del");
+
+    del.forEach(delTask=>{
+        delTask.addEventListener('click', ()=>{
+        let delLi=delTask.parentElement;
+        delLi.remove();
+       })
+    })
+
+
+})
